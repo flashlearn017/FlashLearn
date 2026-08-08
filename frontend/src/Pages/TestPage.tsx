d0508f0bfa35493dbcb29dcbcfbea80858455231
wangv69 <viiwang69@gmail.com>
2026-08-07 23:28:36 -0400

    Displayed all the tests for the user and allows them to choose the test to take.

--- frontend/src/Pages/TestPage.tsx
+++ frontend/src/Pages/TestPage.tsx
@@ -5,42 +5,71 @@ import {useNavigate} from "react-router-dom";
 import {supabase} from "../supabase"
 
 export default function TestPage(){
-    return <Test/>;
-}
-
-function Test(){
-    const [answers, setAnswers] = useState({})
-    const [currentTime, setTime] = useState()
-    const [questions, setQuestions] = useState([])
-    const [timed, setTimed] = useState(false)
-    const navigate = useNavigate();
+    const [test, setTest] = useState([])
+    const [selectedTest, setSelected] = useState(null)
 
     useEffect(() => {
-        async function fetchQuestions(){
+        //displaying all the tests the user has made 
+        async function fetchTest(){
+            const {
+                data: {user},
+            }=await supabase.auth.getUser();
+
             const { data, error} = await supabase
                 .from("Test")
                 .select("*")
-                .eq("id", 6) // temp hard coded , will find a way to display all the tests the user has made then allow them to choose 
-                .single();
+                .eq("user_id", user.id)
 
-                if(error){
-                    console.log(error)
-                    return
-                }
+            if(error){
+                console.log(error)
+                return
+            }
 
-            setQuestions(data.questions)
-            setTimed(data.timedTest)
-            setTime(data.testTime * 60)
+            setTest(data)
         }
-
-        fetchQuestions();
+        
+        fetchTest()
     }, [])
+
+    //the user chooses a test
+    if(!selectedTest){
+        return(
+            <div>
+                <Toolbar/>
+                <div className="flex justify-center items-center min-h-screen flex-col gap-4">
+                    <h1 className="text-4xl text-bold"> Choose a Test to Take</h1>
+                    
+                    {test.map((currentTest, index) => (
+                        <button
+                            key={currentTest.id}
+                            onClick={() => setSelected(currentTest)}
+                            className="text-3xl bg-black text-white rounded-4xl px-3 py-2 hover:bg-slate-400">
+                            Test{index+1}
+                        </button>
+                    ))}
+                </div>
+            </div>
+        )
+    }
+
+    return <Test test={selectedTest}/>;
+}
+
+function Test({test}){
+    const [answers, setAnswers] = useState({})
+    const [currentTime, setTime] = useState()
+    const questions = test.questions
+    const timed = test.timedTest
+
+    const navigate = useNavigate();
     
     useEffect(() => {
         console.log(timed)
         if(!timed){
             return;
         }
+
+        setTime(test.testTime*60)
         
         let interval = setInterval(() => {
             setTime(prev => {
