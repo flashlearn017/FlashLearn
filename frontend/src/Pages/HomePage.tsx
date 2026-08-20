3d0ca1a306f505944c14099d7a430edb78f61e23
Kevin <zhukevinapp@gmail.com>
2026-08-20 15:34:44 -0400

    simple home page that navigates to flashcard and test page

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -9,26 +9,27 @@ function Home(){
     const navigate = useNavigate();
 
     return(
-      <div>
-        <Toolbar/>
-        <div className="flex items-center justify-center h-screen flex-col gap-4">
-          <h1 className="text-3xl font-bold">Welcome to FlashLearn</h1>
-          <p>Create flashcards and test yourself.</p>
+        <div>
+            <Toolbar/>
+            <h1>Home page</h1>
+            <div className="flex items-center justify-center h-screen flex-col gap-4">
+                <h1 className="text-3xl font-bold">Welcome to FlashLearn</h1>
+                <p>Create flashcards and test yourself.</p>
 
-          <button
-            className="bg-black text-white rounded-lg px-3 py-2 hover:bg-slate-400"
-            onClick={() => navigate("/flashcard")}
-          >
-            Flashcards
-          </button>
+                <button
+                    className="bg-black text-white rounded-lg px-3 py-2 hover:bg-slate-400"
+                    onClick={() => navigate("/flashcard-home")}
+                >
+                    Flashcards
+                </button>
 
-          <button
-            className="bg-black text-white rounded-lg px-3 py-2 hover:bg-slate-400"
-            onClick={() => navigate("/test")}
-          >
-            Tests
-          </button>
+                <button
+                    className="bg-black text-white rounded-lg px-3 py-2 hover:bg-slate-400"
+                    onClick={() => navigate("/test-home")}
+                >
+                    Tests
+                </button>
+            </div>
         </div>
-      </div>
     )
 }
